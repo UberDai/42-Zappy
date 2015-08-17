12cdbd8f72b53d35218d5112fcd985191738d397
UberDai <arthimaurer@gmail.com>
2015-08-17 22:05:03 +0200

    ftlst

---
+++ server/ftlst/lst_destroy.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lst_destroy.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsierra- <nsierra-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/04/12 03:55:44 by nsierra-          #+#    #+#             */
+/*   Updated: 2015/04/12 03:56:28 by nsierra-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ftlst.h"
+#include <stdlib.h>
+
+void					lst_destroy(t_lst **lst, void (*f)(void *))
+{
+	void				*data;
+	t_lst				*list;
+
+	list = *lst;
+	while ((data = lst_pop_back(list)))
+	{
+		if (f)
+			f(data);
+	}
+	free(list);
+	*lst = NULL;
+}
